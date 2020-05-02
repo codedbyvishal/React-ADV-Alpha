3ca7d24e0a25c6cded4fcf3836ebfa24441e539a
Vishal Rai <55129439+codedbyvishal@users.noreply.github.com>
2020-05-02 13:58:51 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import FontAwesomeIcon from "./components/FontAwesomeIcon";
+
+export default class App extends Component {
+  render() {
+    return <FontAwesomeIcon icon="align-left" size="6x" color="green" />;
+  }
+}
